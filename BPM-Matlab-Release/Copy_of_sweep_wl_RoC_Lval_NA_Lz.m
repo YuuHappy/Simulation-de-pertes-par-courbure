6f8db770686caf9f97ceb64289da1f9534eb6515
jlabrecque1 <jules.labrecque@indie.inc>
2026-07-13 16:38:32 -0400

    modif pour injection linéaire, tests sans succès pour que la fig init ne remette pas n_unbent chaque fois...

--- BPM-Matlab-Release/Copy_of_sweep_wl_RoC_Lval_NA_Lz.m
+++ BPM-Matlab-Release/Copy_of_sweep_wl_RoC_Lval_NA_Lz.m
@@ -31,7 +31,7 @@ fprintf('Results will be saved in:\n%s\n\n',folderName);
 %% 🔁 Sweep parameters
 %% -------------------------------
 lambda_values = linspace(1060e-9, 1060e-9, 1);
-Lval_values   = linspace(75e-6, 75e-6, 1);
+Lval_values   = linspace(45e-6, 45e-6, 1);
 RoC_values    = linspace(27.5e-3, 27.5e-3, 1);
 NA_values     = linspace(0.0779, 0.0779, 1); 
 Lz_values     = linspace(1e-2, 1e-2, 1);  
@@ -102,10 +102,16 @@ for iz = 1:length(Lz_values)
 
     P = initializeRIfromFunction(P,@calcRI,{n_core,a_core,n_clad,a_clad});
 
+    %% Paramètres initiaux
+
+    P.bendingRoC = Inf;
+    P.lambda = lambda;
+    P.Lz = Lz;
+
     %% Modes
     P.bendDirection = 0;
-    P.bendingRoC = Inf;
     P = findModes(P,8);
+    P.modes(1)
     P.n_eff = P.modes(1).neff;
     fprintf('N_eff is %.5f\n', P.n_eff);
 
@@ -113,9 +119,7 @@ for iz = 1:length(Lz_values)
     P.E = P.modes(1);
 
     %% Assign parameters
-    P.lambda = lambda;
     P.bendingRoC = RoC;
-    P.Lz = Lz;
 
     %% Air cladding
     %P.n_background = n_fa;
